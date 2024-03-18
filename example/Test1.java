527a4f60b313401b24185b8973e12089a2ca35e1
kapil55555 <theharshitkumar500@gmail.com>
2024-03-19 04:48:11 +0530

    first commit test1 java

---
+++ example/Test1.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Test1 {
+	   public static void main(String[] args) {
+		String hungry = "yes";
+		if (hungry == "yes") {
+			System.out.println("Eat something yaarrr...");
+		}
+		else {
+			System.out.println("Do work...");
+		}
+		System.out.println("program running acche se good");
+		
+	}
+	}
